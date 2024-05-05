53765b5906dddd28340e6b2a0d0e7266d8226e73
Jose <jose.maza@hotmail.es>
2024-05-05 11:11:49 +0200

    Commit JMP

---
+++ A01_ControlVersiones/src/josemazapaton/JoseMazaPaton.java
@@ -0,0 +1,13 @@
+package josemazapaton;
+
+public class JoseMazaPaton {
+
+	public static void main(String[] args) {
+		saludar();
+
+	}
+	
+	public static void saludar() {
+		System.out.println("Hola, soy Jose.");
+	}
+}
